b1d0428cc86a1327400caa36ab9fe0eca28a3ee2
“Irina <irina.s.yurtaeva@gmail.com>
2024-09-16 06:41:33 +0200

    phpyurta: cli-table: readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# cli-table
+# CLI Table Generator
+
+## Installation
+
+```
+composer require phpyurta/cli-table
+```
+
+Using
+```
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PHPYurta\CLI\CLITable;
+
+(new CLITable())
+    ->setCaption('Default table ')
+    ->addHeader('~-(^._.^)')
+    ->addHeader('Lifespan')
+    ->addHeader('Origin')
+    ->addHeader('Other names')
+    ->addRow()
+        ->addCell('Maine Coon')
+        ->addCell('13 – 14 years')
+        ->addCell('Maine, United States')
+        ->addCell('Coon Cat, Maine Cat, Maine Shag')
+    ->addRow()
+        ->addCell('Persian cat')
+        ->addCell('12 – 17 years')
+        ->addCell('Iran, Afghanistan')
+        ->addCell('Persian Longhair, Shirazi')
+    ->addRow()
+        ->addCell('Bengal cat')
+        ->addCell('12 – 16 years')
+        ->addCell('United States')
+        ->addCell('I do not know :)')
+    ->printOut()
+;
+```
--- example.php
+++ example.php
@@ -3,8 +3,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PHPYurta\CLI\CLITable;
+
 (new CLITable())
-    ->setCaption('Default')
+    ->setCaption('Default table')
     ->addHeader('~-(^._.^)')
     ->addHeader('Lifespan')
     ->addHeader('Origin')
@@ -15,15 +16,15 @@ use PHPYurta\CLI\CLITable;
         ->addCell('Maine, United States')
         ->addCell('Coon Cat, Maine Cat, Maine Shag')
     ->addRow()
-    ->addCell('Persian cat')
-    ->addCell('12 – 17 years')
-    ->addCell('Iran, Afghanistan')
-    ->addCell('Persian Longhair, Shirazi')
+        ->addCell('Persian cat')
+        ->addCell('12 – 17 years')
+        ->addCell('Iran, Afghanistan')
+        ->addCell('Persian Longhair, Shirazi')
     ->addRow()
-    ->addCell('Bengal cat')
-    ->addCell('12 – 16 years')
-    ->addCell('United States')
-    ->addCell('I do not know :)')
+        ->addCell('Bengal cat')
+        ->addCell('12 – 16 years')
+        ->addCell('United States')
+        ->addCell('I do not know :)')
     ->printOut()
 ;
 
